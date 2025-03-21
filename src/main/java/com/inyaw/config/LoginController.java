8745e9c1012a42582dda09ca14d10c6d8f9425a1
a3318375 <184608371@qq.com>
2025-03-21 14:53:37 +0800

    版本升级

--- src/main/java/com/inyaw/config/LoginController.java
+++ src/main/java/com/inyaw/config/LoginController.java
@@ -1,60 +1,61 @@
-//package com.inyaw.config;
-//
-//import com.inyaw.base.BaseResult;
-//import com.inyaw.sys.bean.InyawSysUser;
-//import com.inyaw.sys.service.InyawSysUserService;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.oauth2.jwt.JwtClaimsSet;
-//import org.springframework.security.oauth2.jwt.JwtEncoder;
-//import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.time.Instant;
-//import java.util.stream.Collectors;
-//
-//@RestController
-//@RequiredArgsConstructor
-//@Slf4j
-//public class LoginController {
-//
-//    private final JwtEncoder encoder;
-//    private final InyawSysUserService inyawSysUserService;
-//    private final AuthenticationManager authenticationManager;
-//
-//    @PostMapping("/token")
-//    public BaseResult<String> token(@RequestBody InyawSysUser user) {
-//        Authentication authResult = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-//                user.getUsername(),
-//                user.getPassword()
-//        ));
-//        Instant now = Instant.now();
-//        long expiry = 36000L;
-//        // @formatter:off
-//        String scope = authResult.getAuthorities().stream()
-//                .map(GrantedAuthority::getAuthority)
-//                .collect(Collectors.joining(" "));
-//        JwtClaimsSet claims = JwtClaimsSet.builder()
-//                .issuer("self")
-//                .issuedAt(now)
-//                .expiresAt(now.plusSeconds(expiry))
-//                .subject(authResult.getName())
-//                .claim("scope", scope)
-//                .build();
-//        // @formatter:on
-//        return BaseResult.success(this.encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue());
-//    }
-//
-//    @PostMapping("/register")
-//    public BaseResult<String> register(@RequestBody InyawSysUser user) {
-//        inyawSysUserService.save(user, true);
-//        // @formatter:on
-//        return BaseResult.success();
-//    }
-//}
+package com.inyaw.config;
+
+import com.inyaw.base.BaseResult;
+import com.inyaw.sys.bean.SysUser;
+import com.inyaw.sys.dto.SysUserDto;
+import com.inyaw.sys.service.SysUserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class LoginController {
+
+    private final JwtEncoder encoder;
+    private final SysUserService sysUserService;
+    private final AuthenticationManager authenticationManager;
+
+    @PostMapping("/token")
+    public BaseResult<String> token(@RequestBody SysUser user) {
+        Authentication authResult = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                user.getUsername(),
+                user.getPassword()
+        ));
+        Instant now = Instant.now();
+        long expiry = 36000L;
+        // @formatter:off
+        String scope = authResult.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(expiry))
+                .subject(authResult.getName())
+                .claim("scope", scope)
+                .build();
+        // @formatter:on
+        return BaseResult.success(this.encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue());
+    }
+
+    @PostMapping("/register")
+    public BaseResult<String> register(@RequestBody SysUserDto user) {
+        sysUserService.save(user, true);
+        // @formatter:on
+        return BaseResult.success();
+    }
+}
