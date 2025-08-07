43538675340f165cd5478dac7e5677f0495ace86
a3318375 <184608371@qq.com>
2025-08-07 17:48:45 +0800

    跨域修改

--- src/main/java/com/inyaw/config/CorsConfig.java
+++ src/main/java/com/inyaw/config/CorsConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,5 +25,21 @@ public class CorsConfig {
         corsConfiguration.addAllowedMethod("*");
         urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
         return urlBasedCorsConfigurationSource;
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // 允许所有请求路径跨域访问
+                        .allowedHeaders("*") // 允许的请求头类型
+                        .maxAge(3600)  // 预检请求的缓存时间（单位：秒）
+                        .allowedMethods("*") // 允许的请求方法类型
+                        .allowedOrigins("*"); // 允许哪些域名进行跨域访问
+            }
+
+        };
     }
 }
